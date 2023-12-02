5721d04514040d8e2e9247ee24036e843fe59fb3
stron <yaneliyahu83@gmail.com>
2023-12-02 12:41:52 +0200

    Open CodedBy links in new tabs

--- src/components/UI/CodedBy/CodedBy.tsx
+++ src/components/UI/CodedBy/CodedBy.tsx
@@ -4,10 +4,13 @@ export default function CodedBy() {
   return (
     <p className="coded-by">
       Challenge by&nbsp;
-      <a href="https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca">
+      <a
+        href="https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca"
+        target="_blank"
+      >
         Frontend Mentor
       </a>
-      . Coded by <a href="https://github.com/itsyanQA">Yan Eliyahu</a>.
+      . Coded by <a href="https://github.com/itsyanQA" target="_blank">Yan Eliyahu</a>.
     </p>
   );
 }
